b8c3360cc2e4ba611c6538308ad4108e8f624ce9
Bamboo0206 <zhuqianye2017@163.com>
2019-04-30 20:40:29 +0800

    提交存档，似乎发现了master的bug?

--- 单词消除游戏/start_game.cpp
+++ 单词消除游戏/start_game.cpp
@@ -5,6 +5,8 @@
 /*游戏每一关，程序会根据该关卡难度，显示一个单词，一定时间后单词消失。
 闯关者需要在相应地方输入刚刚显示并消失的单词，如果闯关者输入正确则为通过。*/
 
+bool one_round(int pass_count);//一关，闯关成功返回true
+
 void start_game()
 {
 	bool finish = false;
@@ -18,69 +20,12 @@ void start_game()
 
 	while (!finish)
 	{
-		cout << "请选择难度（1-5）：" << endl;
-		cin >> difficulty;
-		if (!cin)//输入正确性检验
+		if (one_round(it_user_player->show_pass_count()))
 		{
-			cerr << "input error!\n";
-			cin.clear();
-			cin.ignore(99999, '\n');//放弃包含换行符的输入流中的所有内容
-			continue;
+			
 		}
 
-		cout << "请记住这个单词（3s后消失）：" ;
 
-		int loc = 0;//下标
-		loc = rand() % (size / 5);
-		switch (difficulty)
-		{
-		case 1://随机取前1/5单词
-			break;
-		case 2:
-			loc += size * 1 / 5;
-			break;
-		case 3:
-			loc += size * 2 / 5;
-			break;
-		case 4:
-			loc += size * 3 / 5;
-			break;
-		case 5:
-			loc += size * 4 / 5;
-			break;
-		default:
-			cout << "非法输入" << endl;
-			continue;//??
-			break;
-		}
-
-		/*输出单词*/
-		cout << word_set.at(loc);
-		Sleep(3000);
-
-		cout << "\r                                                            ";
-		cout<<"\r请输入刚才出现的单词：";
-		cin >> input_word;
-		/*输入正确性检验*/
-		if (!cin)
-		{
-			cerr << "input error!\n";
-			cin.clear();
-			cin.ignore(99999, '\n');//放弃包含换行符的输入流中的所有内容
-			continue;
-		}
-
-		if (input_word == word_set.at(loc))//正确
-		{
-			cout << "输入单词正确，闯关成功\n";
-			/*更新等级、经验*/
-			it_user_player->update_EXP(difficulty);
-			it_user_player->update_level();
-		}
-		else//错误
-		{
-			cout << "输入单词错误，闯关失败\t刚才显示的单词是：" << word_set.at(loc) << endl;
-		}
 
 		cout << "请选择：继续游戏0/退出1（默认继续）\n";
 		cin >> finish;
@@ -111,4 +56,101 @@ void print_test_maker()
 		<< "level:" << it_user_test_maker->show_level() << '\t'
 		<< "EXP:" << it_user_test_maker->show_EXP() << '\t'
 		<< "输入单词数:" << it_user_test_maker->show_word_num() << endl;
+}
+
+bool one_round(int pass_count)//一关，闯关成功返回true
+{
+	/*计算单词长度（5级），单词个数，显示时间*/
+	int difficulty = 1, word_num_to_pass = 0, display_time = 0, error_chance = 0;
+	difficulty = pass_count / 3;
+	difficulty = difficulty < 5 ? difficulty : 5;//上限5级
+	word_num_to_pass = pass_count + 1;
+	display_time = -pass_count * 2000 / 15 + 3000;
+	display_time = display_time > 1000 ? display_time : 1000;//下限1000
+	error_chance = -pass_count * 2 / 15;
+	error_chance = error_chance > 1 ? error_chance : 1;//下限1
+		/*输出*/
+	cout << "难度：" << difficulty << "级，单词：" << word_num_to_pass << "个，显示时间：" << display_time << "毫秒" << endl
+		<< "您有" << error_chance << "次错误机会\n";
+
+	string input_word;//用户输入的单词
+	bool finish = false, pass = true;
+	int loc = 0, wordlib_size = 0;//下标,单词数量
+	wordlib_size = word_set.size();
+
+
+	pass = true;
+	for (int i = 1; i <= word_num_to_pass; i++)
+	{
+		/*计算单词下标*/
+		loc = rand() % (wordlib_size / 5);
+		switch (difficulty)
+		{
+		case 1://随机取前1/5单词
+			break;
+		case 2:
+			loc += wordlib_size * 1 / 5;
+			break;
+		case 3:
+			loc += wordlib_size * 2 / 5;
+			break;
+		case 4:
+			loc += wordlib_size * 3 / 5;
+			break;
+		case 5:
+			loc += wordlib_size * 4 / 5;
+			break;
+		default:
+			break;
+		}
+
+		/*输出单词*/
+		cout << "请记住这个单词（<<" << display_time << "毫秒后消失）："
+			<< word_set.at(loc);
+		Sleep(display_time);
+
+
+		cout << "\r                                                            ";
+		cout << "\r请输入刚才出现的单词：";
+		cin >> input_word;
+		/*输入正确性检验*/
+		if (!cin)
+		{
+			cerr << "input error!\n";
+			cin.clear();
+			cin.ignore(99999, '\n');//放弃包含换行符的输入流中的所有内容
+			continue;
+		}
+
+		if (input_word == word_set.at(loc))//正确
+		{
+			cout << "输入单词正确\n";
+
+		}
+		else//错误
+		{
+			cout << "输入单词错误，闯关失败\t刚才显示的单词是：" << word_set.at(loc) << endl;
+			if (--error_chance == -1)
+			{
+				pass = false;
+				break;
+			}
+			cout << "您还有" << error_chance << "次错误机会" << endl;
+		}
+	}
+	/*更新等级、经验*/
+	if (pass)
+	{
+		cout << "闯关成功\n";
+		it_user_player->inc_pass_count();
+		it_user_player->update_EXP(difficulty);//????
+		it_user_player->update_level();
+		return true;
+	}
+	else
+	{
+		cout << "闯关失败" << endl;
+		return false;
+	}
+
 }
